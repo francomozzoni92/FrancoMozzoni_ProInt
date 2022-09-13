324c5162dc297ef0125163a3acb0e5df8f16851a
francomozzoni92 <100097146+francomozzoni92@users.noreply.github.com>
2022-09-12 23:45:48 -0300

    terminando el login

--- FrancoMozzoni_ProInt/Front End/src/app/app.module.ts
+++ FrancoMozzoni_ProInt/Front End/src/app/app.module.ts
@@ -15,6 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 
 
@@ -40,9 +42,12 @@ import { LoginComponent } from './components/login/login.component';
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule,
+    FormsModule
   ],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
