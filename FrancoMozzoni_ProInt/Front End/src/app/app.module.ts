b9d49f99e21f085fe534bba2ed959af128a9d236
francomozzoni92 <100097146+francomozzoni92@users.noreply.github.com>
2022-09-17 20:39:31 -0300

    experiencia terminada

--- FrancoMozzoni_ProInt/Front End/src/app/app.module.ts
+++ FrancoMozzoni_ProInt/Front End/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { FormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
+import { NewExperienciaComponent } from './components/experiencia/new-experiencia.component';
+import { EditExperienciaComponent } from './components/experiencia/edit-experiencia.component';
 
 
 
@@ -35,7 +37,9 @@ import { interceptorProvider } from './service/interceptor-service';
     ProyectosComponent,
     FooterComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NewExperienciaComponent,
+    EditExperienciaComponent
   ],
   imports: [
     BrowserModule,
