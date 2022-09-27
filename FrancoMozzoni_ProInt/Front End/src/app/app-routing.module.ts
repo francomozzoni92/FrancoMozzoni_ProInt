5d1ec1e3655efb96d11b43d1d2eddad6bab62585
francomozzoni92 <100097146+francomozzoni92@users.noreply.github.com>
2022-09-26 21:09:20 -0300

    reformas

--- FrancoMozzoni_ProInt/Front End/src/app/app-routing.module.ts
+++ FrancoMozzoni_ProInt/Front End/src/app/app-routing.module.ts
@@ -8,12 +8,12 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'login', component: LoginComponent},
-  {path:'nuevaexp', component: NewExperienciaComponent},
-  {path:'editexp/:id', component: EditExperienciaComponent},
-  {path:'nuevaedu', component: NeweducacionComponent},
-  {path:'editedu/:id', component: EditeducacionComponent}
+  {path:'', component:HomeComponent},
+  {path:'login', component:LoginComponent},
+  {path:'nuevaexp', component:NewExperienciaComponent},
+  {path:'editexp/:id', component:EditExperienciaComponent},
+  {path:'nuevaedu', component:NeweducacionComponent},
+  {path:'editedu/:id', component:EditeducacionComponent}
 ];
 
 @NgModule({
